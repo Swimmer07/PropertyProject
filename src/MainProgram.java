1944dd895148c8e47dc194c8a28835ecb8fd293f
pisksand <pisksand@kkg-do.eu>
2022-08-31 15:06:59 +0200

    agfr

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -16,13 +16,13 @@ public class MainProgram {
         Property p5 = new Property("Erzbergerstraße 5",5,15);
         System.out.println(p5.getAddress() + " - " + p5.getSize() + "m²");
 
-        Cars q1 = new Cars("Porsche",100.000);
+        Cars q1 = new Cars("Erzbergerstraße 1 - Porsche",100.000);
         System.out.println(q1.getCars() + " - " + q1.getCoast());
 
-        Cars q2 = new Cars("Lambo",230.000);
+        Cars q2 = new Cars("Erzbergerstraße 3 - Lambo",230.000);
         System.out.println(q2.getCars() + " - " + q2.getCoast());
 
-        Cars q3 = new Cars("VW Up",1);
+        Cars q3 = new Cars("Erzbergerstraße 5 - VW Up",1);
         System.out.println(q3.getCars() + " - " + q3.getCoast());
 
     }
