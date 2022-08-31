5170475df11b67ba09f521e86883891a7cbd6ee4
pisksand <pisksand@kkg-do.eu>
2022-08-31 15:05:51 +0200

    agfr

---
+++ src/Cars.java
@@ -0,0 +1,17 @@
+public class Cars {
+    private String cars;
+    private double coast;
+
+    public Cars(String cars, double coast){
+        this.cars = cars;
+        this.coast = coast;
+    }
+
+    public String getCars(){
+        return cars;
+    }
+
+    public double getCoast(){
+        return coast;
+    }
+}
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -16,6 +16,15 @@ public class MainProgram {
         Property p5 = new Property("Erzbergerstraße 5",5,15);
         System.out.println(p5.getAddress() + " - " + p5.getSize() + "m²");
 
+        Cars q1 = new Cars("Porsche",100.000);
+        System.out.println(q1.getCars() + " - " + q1.getCoast());
+
+        Cars q2 = new Cars("Lambo",230.000);
+        System.out.println(q2.getCars() + " - " + q2.getCoast());
+
+        Cars q3 = new Cars("VW Up",1);
+        System.out.println(q3.getCars() + " - " + q3.getCoast());
+
     }
 
 }
